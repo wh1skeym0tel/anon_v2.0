2fe8e05d4fb9563b1fbb9f9f34ab1563da5f9cbf
wh1keym0tel <travellerw3@gmail.com>
2017-08-20 18:51:13 +0300

    update

--- bot_main.py
+++ bot_main.py
@@ -9,37 +9,51 @@ def anon(message):
 	resp = ''
 	if message.text[-2:] == 'да' or message.text[-2:] == 'Да' or message.text[-2:] == 'ДА' or message.text[-2:] == 'дА':
 		resp = 'пакет говна'
+		bot.reply_to(message, resp, parse_mode = 'HTML')
 	elif message.text[-3:] == 'дыа' or message.text[-3:] == 'Дыа':
 		resp = 'пакет говныа'
+		bot.reply_to(message, resp, parse_mode = 'HTML')
 	elif message.text[-3:] == 'нет' :
 		resp = 'говна пакет'
+		bot.reply_to(message, resp, parse_mode = 'HTML')
 	elif message.text[-3:] == 'нетик' :
 		resp = 'говна пакетик'
+		bot.reply_to(message, resp, parse_mode = 'HTML')
 	elif message.text[-3:] == 'нит':
 		resp = 'гавна пакит'
+		bot.reply_to(message, resp, parse_mode = 'HTML')
 	elif message.text[-2:] == 'da':
 		resp = 'paket govna'
+		bot.reply_to(message, resp, parse_mode = 'HTML')
 	elif message.text[-4:] == 'niet' :
 		resp = 'govna pakiet'
+		bot.reply_to(message, resp, parse_mode = 'HTML')
 	elif message.text[-4:] == 'nyet':
 		resp = 'govna pakyet'
+		bot.reply_to(message, resp, parse_mode = 'HTML')
 	elif message.text[-3:] == 'net' :
 		resp = 'govna paket'
+		bot.reply_to(message, resp, parse_mode = 'HTML')
 	elif message.text == 'с новым годом' :
 		resp = 'и тебя, пидор'
+		bot.reply_to(message, resp, parse_mode = 'HTML')
 	elif message.text == 'пидора ответ' :
 		resp = '(сладкого)'
+		bot.reply_to(message, resp, parse_mode = 'HTML')
 	elif message.text == 'приветики' :
 		resp = 'пукни в пакетики'
+		bot.reply_to(message, resp, parse_mode = 'HTML')
 	elif message.text[:2] == 'да' and not message.text == 'да':
 		resp = message.text
 		resp = resp.replace('да','пакет говна')
+		bot.reply_to(message, resp, parse_mode = 'HTML')
 	elif message.text[:2] == 'Да' and not message.text == 'Да':
 		resp = message.text
 		resp = resp.replace('Да','пакет говна')
+		bot.reply_to(message, resp, parse_mode = 'HTML')
 	elif message.text == 'пидор' or message.text == 'Пидор':
-		resp = 'сладкий' 
-	bot.reply_to(message, resp, parse_mode = 'HTML')
+		resp = 'сладкий'
+		bot.reply_to(message, resp, parse_mode = 'HTML')
 
 if __name__ == '__main__':
     bot.polling(none_stop=True)
